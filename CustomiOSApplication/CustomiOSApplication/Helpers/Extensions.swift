ef097a674c75d48d9e29e9852ea0c98f41e7627d
Palaneappan Rajalingam <palane1.2008@gmail.com>
2019-11-27 06:52:56 +0530

    Initial commit

---
+++ CustomiOSApplication/CustomiOSApplication/Helpers/Extensions.swift
@@ -0,0 +1,78 @@
+//
+//  Extensions.swift
+//
+//  Created by Palaneappan Rajalingam on 27/11/19.
+//  Copyright © 2019 Papz. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable extension UIView {
+    
+    @IBInspectable @objc var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable @objc var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.shadowColor = uiColor.cgColor
+        }
+        get{
+            guard let color = layer.shadowColor else { return nil }
+            return UIColor(cgColor: color)
+        }    }
+    
+    @IBInspectable var shadowColor: UIColor?{
+        set {
+            guard let uiColor = newValue else { return }
+            layer.shadowColor = uiColor.cgColor
+        }
+        get{
+            guard let color = layer.shadowColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+
+    @IBInspectable var shadowOpacity: Float{
+        set {
+            layer.shadowOpacity = newValue
+        }
+        get{
+            return layer.shadowOpacity
+        }
+    }
+
+    @IBInspectable var shadowOffset: CGSize{
+        set {
+            layer.shadowOffset = newValue
+        }
+        get{
+            return layer.shadowOffset
+        }
+    }
+
+    @IBInspectable var shadowRadius: CGFloat{
+        set {
+            layer.shadowRadius = newValue
+        }
+        get{
+            return layer.shadowRadius
+        }
+    }
+}
---
+++ CustomiOSApplication/CustomiOSApplication/Views/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  CustomiOSApplication
+//
+//  Created by Palaneappan Rajalingam on 27/11/19.
+//  Copyright © 2019 Papz. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func didSignInTapped(_ sender: Any) {
+        
+    }
+}
